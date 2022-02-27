2457debb49f452691882a1ca2420df651989dac8
Sta2yan <s2_stas@mail.ru>
2022-02-27 14:16:28 +0300

    Добавьте файлы проекта.

---
+++ Subsequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNum = 7;
+            int endNum = 98;
+
+            for (; startNum <= endNum; startNum += 7)
+                Console.Write(startNum + " ");
+        }
+    }
+}
